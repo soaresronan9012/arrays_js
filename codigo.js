9d5335d744cc3f0ffeb29a786d19d7b599e3fbaa
Ronan Soares <soaresronansilva@gmail.com>
2023-08-25 21:52:36 -0300

    first version

---
+++ codigo.js
@@ -0,0 +1,30 @@
+let carros = [//array1
+     "civic", 
+     "corsa", 
+     "volks", 
+     "chevrolet"
+    ]; 
+
+console.log(carros[2]); 
+
+let motos = [ //Array2
+    {nome:"xj6", marca:"yamaha"},
+    {nome:"hornet", marca:"honda"},
+    {nome:"biz", marca:"yamaha"},
+    {nome:"r1", marca:"suzuki"},
+    ];
+
+    console.log(motos[2].marca);
+
+
+let objeto = { //objeto
+    nome:"uno", 
+    marca:"fiat",
+    combustível:"gasolina",
+    portas:"4"
+};
+
+console.log(objeto.combustível);
+
+
+
